f9acf5aaf0bd6e79a8d0d28c2927fc78312f4bf4
seab3115 <139766529+seab3115@users.noreply.github.com>
2024-11-23 14:27:31 +0530

    Add files via upload

---
+++ verify.js
@@ -0,0 +1,19 @@
+// verifyOTP.js
+document.addEventListener('DOMContentLoaded', () => {
+    const verifyOTPForm = document.getElementById('verifyOTPForm');
+
+    if (verifyOTPForm) {
+        verifyOTPForm.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const email = verifyOTPForm.elements.email.value;
+            const otp = verifyOTPForm.elements.otp.value;
+            const response = await fetch(`/api/verifyOTP?email=${email}&otp=${otp}`);
+            const result = await response.json();
+            // console.log(result);
+            alert(result.message);
+            if (result.success) {
+                window.location.href = 'success.html';
+            }
+        });
+    }
+});
